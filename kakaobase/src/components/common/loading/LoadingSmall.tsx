dc8bfddfdaf2fcef080d6e16f8844f788f2fb1a9
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-06-03 20:17:02 +0900

    [FIX] 로딩 중 ui 수정
    - 로딩 중 ui 수정

--- kakaobase/src/components/common/loading/LoadingSmall.tsx
+++ kakaobase/src/components/common/loading/LoadingSmall.tsx
@@ -4,7 +4,6 @@ export default function LoadingSmall() {
   return (
     <div className="text-xs flex flex-col justify-center text-center items-center gap-2 text-textColor">
       <LoaderCircle width={12} height={12} className="animate-spin" />
-      <div>로딩 중...</div>
     </div>
   );
 }
