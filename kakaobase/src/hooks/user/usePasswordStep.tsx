f118a12359c1251de4fa05e4ff176600ba722681
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-06-03 20:15:02 +0900

    [FEAT] 비밀번호 변경 로직 추가
    - 비밀번호 변경 로직 추가
    - password 커스텀 훅

--- kakaobase/src/hooks/user/usePasswordStep.tsx
+++ kakaobase/src/hooks/user/usePasswordStep.tsx
@@ -2,11 +2,14 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { passwordConfirmSchema } from '@/schemas/passwordConfirmSchema';
 import { z } from 'zod';
+import changePassword from '@/apis/changePassword';
+import { useState } from 'react';
 
 export type PasswordFormData = z.infer<typeof passwordConfirmSchema>;
 
 export const usePasswordStep = () => {
-  return useForm<PasswordFormData>({
+  const [loading, setLoading] = useState(false);
+  const methods = useForm<PasswordFormData>({
     resolver: zodResolver(passwordConfirmSchema),
     mode: 'all',
     reValidateMode: 'onChange',
@@ -15,4 +18,21 @@ export const usePasswordStep = () => {
       confirm: '',
     },
   });
+
+  async function onSubmit({ email }: { email: string }) {
+    try {
+      setLoading(true);
+      await changePassword({
+        email: email,
+        password: methods.getValues('password'),
+      });
+      alert('비밀번호 변경 완료');
+    } catch (e: any) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { ...methods, onSubmit, loading };
 };
