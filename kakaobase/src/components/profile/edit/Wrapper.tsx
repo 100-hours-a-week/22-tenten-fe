626a125a56b15799baad2f599ab38e3423d4805a
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-06-03 19:18:09 +0900

    [FIX] 프로필 편집 페이지 ui 조금 수정
    프로필 편집 페이지 ui 조금 수정
    - 비밀번호 변경 페이지와 컨테이너 너비 맞추기

--- kakaobase/src/components/profile/edit/Wrapper.tsx
+++ kakaobase/src/components/profile/edit/Wrapper.tsx
@@ -7,7 +7,7 @@ import TopArea from './TopArea';
 export default function Wrapper() {
   return (
     <div className="flex justify-center items-center animate-slide-in flex-col">
-      <div className="flex bg-containerColor px-8 py-10 rounded-xl flex flex-col items-center gap-8">
+      <div className="flex bg-containerColor px-8 py-10 rounded-xl flex flex-col items-center gap-8 max-w-sm">
         <TopArea />
         <div className="flex items-center gap-4 w-full">
           <UserInput label="깃허브 링크" errorMessage="asdf" />
