25f76cb1868505dec7211f8721f77e15a61022dc
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-06-05 09:36:07 +0900

    [FEAT] 팔로잉/팔로워 목록
    - ui 구현 완료
    - api 초안 작성 완료

--- kakaobase/src/components/likeFollow/UserList.tsx
+++ kakaobase/src/components/likeFollow/UserList.tsx
@@ -3,9 +3,11 @@ import UserItem from './UserItem';
 export default function UserList({
   postId,
   postType,
+  userId,
 }: {
   postId?: number;
   postType?: string;
+  userId?: number;
 }) {
   return (
     <div className="flex flex-col my-[6rem] p-2 bg-containerColor rounded-lg flex-grow self-center w-full max-w-sm animate-slide-in overflow-y-auto gap-2">
