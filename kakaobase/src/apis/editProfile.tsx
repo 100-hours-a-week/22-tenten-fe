1b27a17d77012f31f4cec8ed6fd3df75b404ada1
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-06-07 16:39:40 +0900

    [FEAT] 깃허브 링크 수정
    - 깃허브 링크 수정 api
    - 프로필 이미지 수정 api 인증 param 위치 수정 (잘못된 곳에 넣고 있었음)

--- kakaobase/src/apis/editProfile.tsx
+++ kakaobase/src/apis/editProfile.tsx
@@ -1,16 +1,39 @@
 import { getClientCookie } from '@/lib/getClientCookie';
 import api from './api';
 
-export default async function editProfile({ imageUrl }: { imageUrl: string }) {
+export async function editProfile({ imageUrl }: { imageUrl: string }) {
   const accessToken = getClientCookie('accessToken');
   try {
-    const response = await api.put('/users/images', {
-      image_url: imageUrl,
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
+    await api.put(
+      '/users/images',
+      {
+        image_url: imageUrl,
       },
-    });
-    console.log(response);
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+  } catch (e: unknown) {
+    if (e instanceof Error) throw e;
+  }
+}
+
+export async function editGithub({ url }: { url: string }) {
+  const accessToken = getClientCookie('accessToken');
+  try {
+    await api.put(
+      '/users/github-url',
+      {
+        github_url: url,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
   } catch (e: unknown) {
     if (e instanceof Error) throw e;
   }
