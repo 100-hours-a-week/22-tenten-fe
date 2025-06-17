afe5734ac030da52c77efdae09e40e8c5eddba99
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-06-17 15:54:06 +0900

    [RENAME] 필요 없는 파일 제거
    - 신고 기능 없어짐
    - 채팅 목록 페이지 없어짐 항상 로로와의 챗봇 방임

--- kakaobase/src/app/chat/page.tsx
+++ kakaobase/src/app/chat/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     <div className="flex flex-col h-screen">
       <div className="flex-grow">
         <Header label="채팅 목록" />
-        <div>채팅 목록 페이지입니다.</div>
+        <div>로로와의 채팅방에 오신 것을 환영합니다.</div>
       </div>
       <NavBar />
     </div>
