57215443f842174247fa17662315cf1b6cf92ac3
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-06-29 17:38:33 +0900

    [FIX] 로그인 안 된 사용자를 위한 페이지 구현
    - 인터셉터에서 바로 로그인 페이지가 아닌 인증 안 됨 페이지로 이동하도록 함
    - unauthorized 페이지에서 이동할 페이지를 선택하는 모달로 페이지 구현

--- kakaobase/src/features/feeds/posts/hooks/usePostEditorForm.tsx
+++ kakaobase/src/features/feeds/posts/hooks/usePostEditorForm.tsx
@@ -49,12 +49,8 @@ export const usePostEditorForm = () => {
       showToast('게시글 등록 성공! ✌️');
       router.push(`/main`);
     } catch (e: any) {
-      if (e.response.data.error === 'unauthorized') {
-        showToast('로그인이 필요합니다. 😭');
-      } else {
-        showToast('게시글 업로드 실패! 잠시 후 다시 시도해 주세요. 😭');
-        router.push('/main');
-      }
+      showToast('게시글 업로드 실패! 잠시 후 다시 시도해 주세요. 😭');
+      router.push('/main');
     } finally {
       setLoading(false);
     }
