962acf1f3554af0896684f76bab5f3c9dba30cb9
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-06-30 15:25:59 +0900

    [DESIGN/FIX] Nav Bar 수정
    - 게시글 작성 페이지 이동 버튼 ui 수정
    - nav bar 디자인 수정
    - 로그인 안 된 사용자 페이지 ui 수정
    -

--- kakaobase/src/app/unauthorized/page.tsx
+++ kakaobase/src/app/unauthorized/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
 
       <div className="flex h-screen justify-center items-center">
         <div className="m-6">
-          <div className="flex flex-col gap-6 bg-containerColor p-6 rounded-xl">
+          <div className="flex flex-col gap-6 bg-containerColor px-10 py-6 md:px-6 rounded-xl animate-slide-in">
             <div className="flex text-center justify-center">
               로그인이 필요합니다.
               <br />
--- kakaobase/src/widgets/navbar/NavBar.tsx
+++ kakaobase/src/widgets/navbar/NavBar.tsx
@@ -1,25 +1,14 @@
 'use client';
-import { usePathname } from 'next/navigation';
 import { House, MessageCircle, Bell } from 'lucide-react';
 import NavItem from './NavItem';
 import NavItemProfile from './NavItemProfile';
-import { useUserStore } from '@/entities/users/stores/userStore';
-import NavItemPost from './NavItemPost';
 
 export default function NavBar() {
-  const path = usePathname();
-  const { course, selectedCourse } = useUserStore();
-
   return (
     <div className="flex sticky w-full max-w-[480px] border-t-[1px] border-textOpacity50 bottom-0 mx-auto lg:self-start bg-bgColor text-textColor shadow-md">
-      <div className="h-16 bg-bgColor flex justify-around items-end px-8 py-5 w-full z-50">
+      <div className=" bg-bgColor flex justify-around items-center w-full z-50">
         <NavItem icon={House} path="/" />
         <NavItem icon={MessageCircle} path="/chat" />
-        {path.includes('main') &&
-          (selectedCourse === course || selectedCourse === 'ALL') && (
-            <NavItemPost />
-          )}
-
         <NavItem icon={Bell} path="/alarm" />
         <NavItemProfile />
       </div>
