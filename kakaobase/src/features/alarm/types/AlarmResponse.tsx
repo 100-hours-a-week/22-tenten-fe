b2266ce386a615d7482eeca9280aa7674334d3d3
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-07-07 16:14:19 +0900

    [FEAT] 알림 응답 타입 정의
    - fetch, remove, read에 맞는 응답 형태 정의

--- kakaobase/src/features/alarm/types/AlarmResponse.tsx
+++ kakaobase/src/features/alarm/types/AlarmResponse.tsx
@@ -1,6 +1,20 @@
-import { AlarmEvent } from "./AlarmEvent";
+import { AlarmEvent } from './AlarmEvent';
+import { AlarmFetchData } from './AlarmFetchResponse';
 
-export interface AlarmResponse {
-    event: AlarmEvent;
-    data: any;
+export type AlarmResponse =
+  | {
+      type: string;
+      event: 'notification.fetch';
+      data: AlarmFetchData;
+    }
+  | {
+      type: string;
+      event: Exclude<AlarmEvent, 'notification.fetch'>;
+      data: AlarmDefaultData;
+    };
+
+export interface AlarmDefaultData {
+  id: number;
+  message: string;
+  timestamp: string;
 }
