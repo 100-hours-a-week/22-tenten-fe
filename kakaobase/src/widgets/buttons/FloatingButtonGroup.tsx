cddbe138f0406c56b0a9abad19710c37894f9ace
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-07-08 10:32:04 +0900

    [DESIGN] 게시글 작성 버튼 위치 변경
    - 모바일에서 보이지 않는 문제 해결

--- kakaobase/src/widgets/buttons/FloatingButtonGroup.tsx
+++ kakaobase/src/widgets/buttons/FloatingButtonGroup.tsx
@@ -5,7 +5,7 @@ import GoUp from './GoUp';
 
 export default function FloatingButtonGroup() {
   return (
-    <div className="sticky inset-0 pointer-events-none">
+    <div className="sticky inset-0 pointer-events-none h-screen">
       <div className="flex">
         <div className="hidden lg:flex flex-col w-[48%]"></div>
         <div className="flex justify-center max-w-[480px] w-full mx-auto lg:ml-12 lg:self-start">
