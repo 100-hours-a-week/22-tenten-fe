c732910c597788fb32c4e77120684debed6f0e93
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-07-09 16:51:36 +0900

    [DESIGN] 채팅 입력 바 보이게
    - 채팅페이지에서 navbar 삭제

--- kakaobase/src/features/chat/ui/ChatList.tsx
+++ kakaobase/src/features/chat/ui/ChatList.tsx
@@ -2,23 +2,39 @@
 import { useEffect, useState } from 'react';
 import MyChat from './MyChat';
 import BotChat from './BotChat';
-import MessageInput from './MessageInput';
 
 export default function ChatList() {
   const [isMine, setMine] = useState(true);
+
   useEffect(() => {
     const sc = document.querySelector<HTMLElement>('[data-scroll-area]');
     sc?.scrollTo({ top: sc.scrollHeight, behavior: 'auto' });
   }, []);
+
   return (
-    <div className="flex flex-col w-full h-screen overflow-y-auto">
-      <div
-        className="flex flex-col w-full h-screen overflow-y-auto"
-        data-scroll-area
-      >
-        <BotChat />
-      </div>
-      <MessageInput />
+    <div
+      className="flex flex-col w-full h-screen overflow-y-auto"
+      data-scroll-area
+    >
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
+      <BotChat />
     </div>
   );
 }
