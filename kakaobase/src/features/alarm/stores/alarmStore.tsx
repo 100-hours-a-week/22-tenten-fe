e1ea8af9e52209ad79a87958767d03e1f8c87548
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-07-22 14:49:20 +0900

    [FIX] 알림 로직 수정
    - 알림 목록 전역 상태 삭제
    - 알림 개수만 변경 및 데이터 업데이트
    - 유튜브 요약 null 응답일 경우 요약 없음 처리

--- kakaobase/src/features/alarm/stores/alarmStore.tsx
+++ kakaobase/src/features/alarm/stores/alarmStore.tsx
@@ -1,15 +1,11 @@
 import { create } from 'zustand';
-import { AlarmItem } from '../types/AlarmResponse';
 
 const initialAlarmState = {
   cnt: 0,
-  alarmList: [] as AlarmItem[],
 };
 
 export interface AlarmState {
   cnt: number;
-  alarmList: AlarmItem[];
-  setAlarmList: (alarmList: AlarmItem[]) => void;
   setCnt: (cnt: number) => void;
   clear: () => void;
 }
@@ -17,6 +13,5 @@ export interface AlarmState {
 export const useAlarmStore = create<AlarmState>()((set) => ({
   ...initialAlarmState,
   setCnt: (cnt) => set({ cnt: cnt }),
-  setAlarmList: (alarmList) => set({ alarmList: alarmList }),
   clear: () => set({ cnt: 0 }),
 }));
