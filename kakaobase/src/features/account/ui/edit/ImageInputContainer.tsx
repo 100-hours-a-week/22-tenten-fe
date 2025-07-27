93f4e026765dadf0fd0b021cfea0c2965929b2ba
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-07-27 20:50:08 +0900

    [FIX] 이미지 레이블 정리
    - ImageLabel로 container와 ui 분리
    - 기존 구현하였으나 이미지 최적화하면서 재정리

--- kakaobase/src/features/account/ui/edit/ImageInputContainer.tsx
+++ kakaobase/src/features/account/ui/edit/ImageInputContainer.tsx
@@ -30,31 +30,7 @@ export default function ImageInput({
 
   return (
     <div>
-      <label
-        htmlFor="image-upload"
-        className="flex relative gap-2 items-center justify-center cursor-pointer"
-      >
-        {!image || image === 'null' ? (
-          <div className="w-16 h-16 block bg-textOpacity50 rounded-lg"></div>
-        ) : (
-          <div className="relative aspect-square w-16 h-16">
-            <Image
-              src={image}
-              alt="프로필 이미지"
-              className="rounded-lg object-cover"
-              fill
-              sizes="(max-width : 480px) 64px, 13vw"
-            />
-          </div>
-        )}
-        <div className="w-16 h-16 aspect-square bg-textOpacity50 absolute flex rounded-lg justify-center">
-          {loading ? (
-            <LoadingSmall />
-          ) : (
-            <CirclePlus className="w-4 text-textColor self-center" />
-          )}
-        </div>
-      </label>
+      <ImageLabel image={image} loading={loading} />
 
       <input
         id="image-upload"
